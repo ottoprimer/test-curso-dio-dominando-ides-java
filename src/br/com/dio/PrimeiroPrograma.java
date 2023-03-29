a07bf35013b3b49900521df40f5ae4f1af14bac0
ottoprimer <theottoprime@gmail.com>
2023-03-28 23:55:02 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+	Livro livro = new Livro();
+	Gato gato = new Gato();
+	
+	System.out.println(gato);
+	System.out.println(livro);
+		
+	}
+}
